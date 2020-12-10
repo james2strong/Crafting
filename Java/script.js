10e54bd166f7ac186cd1d6078b27cda248072cbe
unknown <james2strong@gmail.com>
2020-12-10 13:53:22 -0500

    added javascript for images

---
+++ Java/script.js
@@ -0,0 +1,70 @@
+
+function imageflip() {
+    var Image_Id = document.getElementById('Barbarian');
+    if (Image_Id.src.match("../Images/Minis/BarbarianFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/BarbarianBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/BarbarianFront.jpeg";
+    }
+}  
+
+function imageflip1() {
+    var Image_Id = document.getElementById('Cowboy');
+    if (Image_Id.src.match("../Images/Minis/CowboyFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/CowboyBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/CowboyFront.jpeg";
+    }
+}  
+
+function imageflip2() {
+    var Image_Id = document.getElementById('Skeleton');
+    if (Image_Id.src.match("../Images/Minis/PurpSkelFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/PurpSkelBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/PurpSkelFront.jpeg";
+    }
+}  
+
+function imageflip3() {
+    var Image_Id = document.getElementById('Minotaur');
+    if (Image_Id.src.match("../Images/Minis/MinotaurFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/MinotaurBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/MinotaurFront.jpeg";
+    }
+} 
+
+function imageflip4() {
+    var Image_Id = document.getElementById('Spider');
+    if (Image_Id.src.match("../Images/Minis/SpiderFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/SpiderBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/SpiderFront.jpeg";
+    }
+}
+
+function imageflip5() {
+    var Image_Id = document.getElementById('Knight');
+    if (Image_Id.src.match("../Images/Minis/KnightFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/KnightBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/KnightFront.jpeg";
+    }
+}
+
+function imageflip6() {
+    var Image_Id = document.getElementById('Orc');
+    if (Image_Id.src.match("../Images/Minis/OrcFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/OrcBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/OrcFront.jpeg";
+    }
+}
