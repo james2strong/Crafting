9a04bb0e60ea1953429f6255583ca43ea8c663d7
unknown <james2strong@gmail.com>
2020-12-17 12:16:30 -0500

    made all imgages flip

--- Javascript/script.js
+++ Javascript/script.js
@@ -20,7 +20,7 @@ function imageflip1() {
 }  
 
 function imageflip2() {
-    var Image_Id = document.getElementById('Skeleton');
+    var Image_Id = document.getElementById('Skeleton1');
     if (Image_Id.src.match("../Images/Minis/PurpSkelFront.jpeg")) {
         Image_Id.src = "../Images/Minis/PurpSkelBack.jpeg";
     }
@@ -66,5 +66,65 @@ function imageflip6() {
     }
     else {
         Image_Id.src = "../Images/Minis/OrcFront.jpeg";
+    }
+}
+
+function imageflip7() {
+    var Image_Id = document.getElementById('Cyclops1');
+    if (Image_Id.src.match("../Images/Minis/Cyclops1.jpeg")) {
+        Image_Id.src = "../Images/Minis/Cyclops1Back.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/Cyclops1.jpeg";
+    }
+}
+
+function imageflip8() {
+    var Image_Id = document.getElementById('Cyclops2');
+    if (Image_Id.src.match("../Images/Minis/Cyclops2.jpeg")) {
+        Image_Id.src = "../Images/Minis/Cyclops2Back.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/Cyclops2.jpeg";
+    }
+}
+
+function imageflip9() {
+    var Image_Id = document.getElementById('FireGiant');
+    if (Image_Id.src.match("../Images/Minis/FireGiantFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/FireGiantBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/FireGiantFront.jpeg";
+    }
+}
+
+function imageflip10() {
+    var Image_Id = document.getElementById('Skeleton2');
+    if (Image_Id.src.match("../Images/Minis/GreenSkelFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/GreenSkelBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/GreenSkelFront.jpeg";
+    }
+}
+
+function imageflip11() {
+    var Image_Id = document.getElementById('Skeleton3');
+    if (Image_Id.src.match("../Images/Minis/RedSkelFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/RedSkelBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/RedSkelFront.jpeg";
+    }
+}
+
+function imageflip12() {
+    var Image_Id = document.getElementById('Golem');
+    if (Image_Id.src.match("../Images/Minis/FleshGolemFront.jpeg")) {
+        Image_Id.src = "../Images/Minis/FleshGolemBack.jpeg";
+    }
+    else {
+        Image_Id.src = "../Images/Minis/FleshGolemFront.jpeg";
     }
 }
